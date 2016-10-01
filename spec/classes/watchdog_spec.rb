ac049d7fa832a067d002b1ebee26cdc2f6c7fad8
Matt Dainty <matt@bodgit-n-scarper.com>
2016-10-02 00:31:01 +0100

    Update tests to use rspec-puppet-facts

--- spec/classes/watchdog_spec.rb
+++ spec/classes/watchdog_spec.rb
@@ -9,50 +9,36 @@ describe 'watchdog' do
       }
     end
 
-    it do
-      expect { subject }.to raise_error(/not supported on an Unsupported/)
-    end
+    it { expect { should compile }.to raise_error(/not supported on an Unsupported/) }
   end
 
-  context 'on RedHat', :compile do
-    let(:facts) do
-      {
-        :osfamily => 'RedHat'
-      }
-    end
+  on_supported_os.each do |os, facts|
+    context "on #{os}", :compile do
+      let(:facts) do
+        facts
+      end
 
-    it do
-      should contain_class('watchdog')
-      should contain_file('/etc/watchdog.conf')
-      should contain_package('watchdog')
-      should contain_service('watchdog').with(
-        'ensure' => 'running',
-        'enable' => true
-      )
-    end
-  end
+      it { should contain_anchor('watchdog::begin') }
+      it { should contain_anchor('watchdog::end') }
+      it { should contain_class('watchdog') }
+      it { should contain_class('watchdog::config') }
+      it { should contain_class('watchdog::install') }
+      it { should contain_class('watchdog::params') }
+      it { should contain_class('watchdog::service') }
 
-  context 'on OpenBSD', :compile do
-    let(:facts) do
-      {
-        :osfamily => 'OpenBSD'
-      }
-    end
-
-    it do
-      should contain_class('watchdog')
-      should contain_service('watchdogd').with(
-        'ensure' => 'running',
-        'enable' => true,
-        'flags'  => '-i 20 -p 60'
-      )
-      should contain_sysctl('kern.watchdog.period').with(
-        'ensure' => 'absent'
-      )
-      should contain_sysctl('kern.watchdog.auto').with(
+      case facts[:osfamily]
+      when 'OpenBSD'
+        it { should contain_service('watchdogd').with_flags('-i 20 -p 60') }
+        it { should contain_sysctl('kern.watchdog.period').with_ensure('absent') }
+        it { should contain_sysctl('kern.watchdog.auto').with(
         'ensure' => 'present',
         'value'  => 0
-      )
+        ) }
+      when 'RedHat'
+        it { should contain_file('/etc/watchdog.conf') }
+        it { should contain_package('watchdog') }
+        it { should contain_service('watchdog') }
+      end
     end
   end
 end
